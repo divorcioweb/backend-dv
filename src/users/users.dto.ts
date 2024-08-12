81d75b02acd57d0eede7927c6f6cc578330e46a4
unknown <2003silvagui@gmail.com>
2024-08-12 14:31:08 -0300

    feat: register user with more informations

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,5 +1,35 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, MinLength, IsEmail } from 'class-validator';
+import { IsString, IsNotEmpty, MinLength, IsEmail, IsBoolean } from 'class-validator';
+
+export class AddressDTO {
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  estado: string;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  cidade: string;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  pais: string;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  cep: string;
+}
 
 export class UserDTO {
   @ApiProperty({
@@ -31,4 +61,64 @@ export class UserDTO {
   @IsNotEmpty()
   @MinLength(8)
   senha: string;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  profissao: string;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  rg: string;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  cpf: string;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  type: string;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  estado_civil: string;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  @IsNotEmpty()
+  naturalidade: string;
+
+  @ApiProperty({
+    example: false,
+  })
+  @IsBoolean()
+  nome_solteiro: boolean;
+
+  @ApiProperty({
+    example: 'string',
+  })
+  @IsString()
+  usuario_id: string;
+
+  @ApiProperty({
+    type: AddressDTO,
+    description: 'Endereço do usuário',
+  })
+  endereco: AddressDTO;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,18 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConnectionService } from 'src/connection/connection.service';
-import { UserDTO } from './users.dto';
 import * as bcrypt from 'bcrypt';
+import { UserDTO } from './users.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly db: ConnectionService) {}
 
   async findAll() {
-    return await this.db.users.findMany();
+    return await this.db.user.findMany();
   }
 
   async register(user: UserDTO) {
-    const userAlreadyRegistered = await this.db.users.findFirst({
+    const userAlreadyRegistered = await this.db.user.findFirst({
       where: {
         email: user.email,
       },
@@ -22,23 +22,54 @@ export class UsersService {
       throw new BadRequestException();
     }
 
-    const userCreated = await this.db.users.create({
+    const userCreated = await this.db.user.create({
       data: {
-        ...user,
+        nome: user.nome,
+        email: user.email,
+        telefone: user.telefone,
+        cpf: user.cpf,
+        rg: user.rg,
+        estado_civil: user.estado_civil,
+        naturalidade: user.naturalidade,
+        nome_solteiro: user.nome_solteiro,
+        profissao: user.profissao,
+        type: user.type,
+        usuario_id: "",
         senha: bcrypt.hashSync(user.senha, 10),
       },
       select: {
         id: true,
         email: true,
         nome: true,
+        cpf: true,
+        rg: true,
+        estado_civil: true,
+        naturalidade: true,
+        nome_solteiro: true,
+        profissao: true,
+        telefone: true,
+        type: true,
+        usuario_id: true,
+        created_at: true,
+        updated_at: true,
       },
     });
 
-    return userCreated;
+    const addressCreated = await this.db.endereco.create({
+      data: {
+        cep: user.endereco.cep,
+        cidade: user.endereco.cidade,
+        estado: user.endereco.estado,
+        pais: user.endereco.pais,
+        userId: userCreated.id,
+      },
+    });
+
+    return { ...userCreated, endereco: addressCreated };
   }
 
   async findByEmail(email: string) {
-    return await this.db.users.findFirst({
+    return await this.db.user.findFirst({
       where: {
         email: email,
       },
